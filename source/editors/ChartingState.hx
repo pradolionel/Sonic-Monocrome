f7bffb6ab030f646fd25f9ac4f0a00cf5ee770e8
pradolionel <99144412+pradolionel@users.noreply.github.com>
2022-06-05 12:21:54 -0500

    Update ChartingState.hx

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -78,7 +78,6 @@ class ChartingState extends MusicBeatState
 		['Alt Idle Animation', "Sets a speciied suffix after the idle animation name.\nYou can use this to trigger 'idle-alt' if you set\nValue 2 to -alt\n\nValue 1: Character to set (Dad, BF or GF)\nValue 2: New suffix (Leave it blank to disable)"],
 		['Screen Shake', "Value 1: Camera shake\nValue 2: HUD shake\n\nEvery value works as the following example: \"1, 0.05\".\nThe first number (1) is the duration.\nThe second number (0.05) is the intensity."],
 		['Change Character', "Value 1: Character to change (Dad, BF, GF)\nValue 2: New character's name"]
-		['Toggle Hell', "Toggles the BG for last straw"],
 		['Toggle HUD alpha', "Toggle the HUD's visibility\nValue 1: the time in seconds for the toggle switch"],
 		['spooky', "oh god it's spinel"],
 		['Black BG Toggle', "Value 1: Type 'on' or 'off'.\nValue 2: Duration of Fade in seconds.",],
